96289ea251227c7cb37a131b0fa7f2d55867ae6d
YDenker <33430355+YDenker@users.noreply.github.com>
2019-12-05 04:50:52 +0100

    Menu completed

---
+++ WWF Game Jam/Assets/Code/Menu/ExitWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitWindow : MenuWindow
+{
+    public override void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public override void Enable()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void TickBox(bool on, int boxID)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ WWF Game Jam/Assets/Code/MenuManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MenuState { START, MAPSELECTION, OPTIONS, EXIT}
+
+public class MenuManager : MonoBehaviour
+{
+
+    private static MenuManager instance;
+    public static MenuManager Instance { get => instance; }
+
+    private MenuState currentMenuState;
+
+    [SerializeField]
+    private MapSelectionWindow msWindow;
+    [SerializeField]
+    private MainMenuWindow mmWindow;
+    [SerializeField]
+    private OptionsWindow oWindow;
+    [SerializeField]
+    private ExitWindow eWindow;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnEnable()
+    {
+        currentMenuState = MenuState.START;
+        mmWindow.Enable();
+        msWindow.Disable();
+        oWindow.Disable();
+        eWindow.Disable();
+    }
+
+    public void ChangeMenuState(int newState)
+    {
+        switch (currentMenuState)
+        {
+            case MenuState.START:
+                mmWindow.Disable();
+                break;
+            case MenuState.MAPSELECTION:
+                msWindow.Disable();
+                break;
+            case MenuState.OPTIONS:
+                oWindow.Disable();
+                break;
+            case MenuState.EXIT:
+                eWindow.Disable();
+                break;
+            default:
+                break;
+        }
+
+        switch ((MenuState)newState)
+        {
+            case MenuState.START:
+                mmWindow.Enable();
+                break;
+            case MenuState.MAPSELECTION:
+                msWindow.Enable();
+                break;
+            case MenuState.OPTIONS:
+                oWindow.Enable();
+                break;
+            case MenuState.EXIT:
+                eWindow.Enable();
+                break;
+            default:
+                break;
+        }
+
+        currentMenuState = (MenuState)newState;
+    }
+}
